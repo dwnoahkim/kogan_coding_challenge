7659eaad9b368d0112a2f35497b70ba7779e0e7f
NoahKim <dongwoo_333@hanmail.net>
2020-06-04 18:51:57 +1000

    Calculate cubic weight on the list and implement List component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,9 +8,30 @@ export const fetchItemsByCategory = (category) => async dispatch => {
       { category }
     );
 
+    let { objects } = response.data;
+    // Calculate objects' cubic weight
+    objects.map(obj => {
+      const conversionFactor = 250;
+      let { width, length, height } = obj.size;
+      let cubicWeight = 0;
+
+      // Unit conversion : cm to m
+      width *= 0.01;
+      length *= 0.01;
+      height *= 0.01;
+
+      // calculate cubic weight
+      cubicWeight = (width * length * height) * conversionFactor;
+      
+      // Unit conversion : kg to g to match with weight
+      cubicWeight = Math.round(cubicWeight * 1000);
+
+      obj.cubicWeight = cubicWeight;
+    });
+
     dispatch({
       type: FETCH_ITEMS_BY_CATEGORY, 
-      payload : response.data.objects
+      payload : objects
     });
   } catch (e) {
     dispatch({
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+  roundTo(num, decimal) {
+    const calcDecimal = Math.pow(10, decimal);
+    return Math.round(num * calcDecimal) / calcDecimal;
+  };
+
+  renderList() {
+    return this.props.list.map(item => {
+      return (
+        <tr key={item.title}>
+          <td>{item.category}</td>
+          <td>{item.title}</td>
+          <td>{item.size.width.toFixed(1)}</td>
+          <td>{item.size.length.toFixed(1)}</td>
+          <td>{item.size.height.toFixed(1)}</td>
+          <td>{this.roundTo(item.weight/1000, 2)}</td>
+          <td>{this.roundTo(item.cubicWeight/1000, 2)}</td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     if (Object.entries(this.props.list).length === 0) {
       return ( <div></div> );
     }
 
-    return this.props.list.map(item => {
-      return (
-        <div key={item.title}>
-          {item.title}
-        </div>
-      );
-    });
+    return (
+      <div className="container">
+        <table className="u-full-width">
+          <thead>
+            <tr>
+              <th>Category</th>
+              <th>Title</th>
+              <th>Width(cm)</th>
+              <th>Length(cm)</th>
+              <th>Height(cm)</th>
+              <th>Weight(kg)</th>
+              <th>Cubic Weight(kg)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderList()}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
